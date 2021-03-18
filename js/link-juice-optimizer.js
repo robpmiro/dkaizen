65ef4e3d5c79ad13bb1e470e65af48ff3e62edf2
robpmiro <45032889+robpmiro@users.noreply.github.com>
2021-03-18 13:47:20 +0100

    cambios

---
+++ js/link-juice-optimizer.js
@@ -0,0 +1,31 @@
+(function ($) {
+	'use strict';
+
+	function ljo_decode(str) {
+		//return unescape(decodeURIComponent(window.atob(str)));
+		return unescape(window.atob(decodeURIComponent(str)));
+	}
+
+	$(document).ready(function () {
+		var ljo_clase = php_vars.ljo_clase;
+		$("." + ljo_clase + " span, span." + ljo_clase).click(function (event) {
+			if ($(this).attr("data-loc") !== undefined && $(this).attr("data-loc") !== false) {
+				if ($(this).attr("data-window") == 'new') {
+					window.open(ljo_decode($(this).attr("data-loc")), '_blank');
+				} else {
+					window.location.href = ljo_decode($(this).attr("data-loc"));
+				}
+			}
+		});
+	});
+
+})(jQuery);
+
+function ljo_open(str, target) {
+	if (target == 'new') {
+		window.open(str, '_blank');
+	} else {
+		window.location.href = str;
+	}
+}
+/*This file was exported by "Export WP Page to Static HTML" plugin which created by ReCorp (https://myrecorp.com) */
